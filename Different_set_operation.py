4ad6603fb567e5e310f0edd2eaa0c63a0f170a78
YashT096 <109242952+YashT096@users.noreply.github.com>
2022-11-15 22:48:36 +0530

    Create Different_set_operation.py
    The following program shows union, intersection, difference and symmetric difference.

---
+++ Different_set_operation.py
@@ -0,0 +1,10 @@
+n={"0","1","2","5","3","6","7","4"}
+e={"2","4","8","9","10"}
+union=n|e
+print(" union is :",union)
+intersection =n|e
+print("intersection is :",intersection)
+difference=e.difference(n)
+print("difference is : ",difference)
+symmetric_difference=e.symmetric_difference(n)
+print("symmetric_difference is :",symmetric_difference)
